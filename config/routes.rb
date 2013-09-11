2472f9eaf2a56d666062ad27d7fd609eb8ea2d57
Rob Phillips <ufbobbo@gmail.com>
2013-09-11 18:48:09 -0400

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+CocoaOfficeHours::Application.routes.draw do
+  
+  root 'application#index'
+  get '/next_event' => 'application#next_event'
+  get '/attendings' => 'application#attendings'
+  get '/auth/github' => 'application#github_auth'
+  get '/auth/github/callback' => 'application#github_callback'
+  
+end
